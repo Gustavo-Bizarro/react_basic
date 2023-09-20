869b9cedbe1d3bbfed485e11e41a85a06a35c2ce
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-09-20 17:11:11 -0300

    desfio do react

---
+++ teste-1/src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+          <h3 className="gust">Desafio 1 criar tag, ok</h3>
+          <p>Hello Word!!!</p>
+        </a>
+      </header>
+    </div>
+    
+  );
+}
+
+export default App;
+
