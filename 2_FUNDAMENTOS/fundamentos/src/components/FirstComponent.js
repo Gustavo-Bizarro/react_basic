6b23fd944f40a22c2d15cb83202d5c2a6a6c926c
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-09-21 10:14:26 -0300

    executando JSX

--- 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/FirstComponent.js
@@ -3,6 +3,7 @@ const FirstComponent = () => {
     return (
         <div>
             <h1>Meu primeiro Componente</h1>
+            <p>Temos sempre que incapsular as tag em uma tag pai, ex div </p>
         </div>
     )
 }
--- 2_FUNDAMENTOS/fundamentos/src/components/SecondComponent.js
+++ 2_FUNDAMENTOS/fundamentos/src/components/SecondComponent.js
@@ -3,6 +3,8 @@ const SecondComponent = () => {
     return (
         <div>
             <h1>Segundo componente</h1>
+            <h1>Terceiro Componente no React</h1>
+            <p>Tudo que está dentro do Return pode ser considerado uma função JSX, ou seja, podemos manipular o html.</p>
         </div>
     )
 }
