025c8a8bfbc3b71476d215a707108cf630c585ed
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-09-25 13:47:33 -0300

    if e else no jsx, usando useState ou mundando o estado do componente

--- 3_AVANCANDO_REACT/avancando_react/src/Components/CondicionalRender.js
+++ 3_AVANCANDO_REACT/avancando_react/src/Components/CondicionalRender.js
@@ -1,12 +1,26 @@
 import { useState } from "react"
 
 const CondicionalRender = () => {
-    const [x] = useState(false)
+  const [x] = useState(false)
+  const [name, setName] = useState("Gustavo")
+
   return (
     <div>
         <h1>isso Será Exibido?</h1>
         {x && <p>Se X for true, sim!</p> }
         {!x && <p>Se X for false, sim!</p> }
+
+        <h1>If Ternário</h1>
+      {name === "Gustavo" ? (
+        <div>
+         <p>Esse é o Gustavo</p>
+        </div>
+      ) : (
+        <div>
+           <p>Não tem registro.</p>
+        </div>
+      )}
+      <button onClick={() => setName("João")}>clicar e confirme</button>
     </div>
   )
 }
