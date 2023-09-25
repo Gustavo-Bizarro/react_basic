8c36527fba73bbdc3ceb7f66e587f69ed6beed07
Gustavo Bizarro Lombardo <90002024+Dev1Bizarro@users.noreply.github.com>
2023-09-25 14:52:59 -0300

    Destructuring

--- 3_AVANCANDO_REACT/avancando_react/src/App.js
+++ 3_AVANCANDO_REACT/avancando_react/src/App.js
@@ -7,13 +7,21 @@ import ManageData from './Components/ManageData';
 import ListRender from './Components/ListRender';
 import CondicionalRender from './Components/CondicionalRender';
 
+//props
+import ShowName from './Components/ShowName';
+
+//Destructuing
+import CarDetails from './Components/CarDetails';
 //import do css
 import './App.css';
+import { useState } from 'react';
 
 
 
 
 function App() {
+  
+  const [userName] = useState("Maria")
   return (
     <div className="App">
       <h1>Avançando no React</h1>
@@ -21,16 +29,23 @@ function App() {
       <div>
     <img src="/img1.jpg" alt="Paisagem" />
       </div>
-      {/*agora vou colocar a imagem em assets*/}
+      {/*colocar a imagem em assets*/}
       <div>
         <img src={City} alt="Cidade durante a noite" />
         <img src={gustavo} alt="Imagem do Gustavo" />
+
         <ManageData/>
         <ListRender/>
         <CondicionalRender/>
 
-      
-      </div>
+        {/*Props*/}z
+        <ShowName name={userName}/>
+
+        {/*Destructuring*/}
+        <CarDetails brand="Nissan" km={0} color="Prata Lunar"/>
+
+
+       </div>
     </div>
   );
 }
